34dc21a402287adc527c2aef57cc8c6ee0ec5a09
”jellywong88“ <wjl_hdu@163.com>
2025-11-06 20:21:44 +0800

    upper lower 分开处理

--- vis_martials/vis_heatmap.py
+++ vis_martials/vis_heatmap.py
@@ -7,6 +7,9 @@ import argparse
 import matplotlib as mpl
 from PIL import Image
 
+
+"可视化上下颌的咬合状态，增加热力分布显示功能，并分上下颌分别截图保存"
+
 def load_mesh(file_path):
     """加载PLY网格文件"""
     mesh = o3d.io.read_triangle_mesh(file_path)
